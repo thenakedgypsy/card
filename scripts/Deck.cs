a2567748f28c551be64d64294bd32157673aedba
Sam <106659952+thenakedgypsy@users.noreply.github.com>
2026-08-16 14:19:46 +0100

    fix: reduce initial cards

--- scripts/Deck.cs
+++ scripts/Deck.cs
@@ -13,11 +13,6 @@ public partial class Deck : Cardpile
 		AddCard("blockOfIce");
 		AddCard("blockOfIce");
 		AddCard("blockOfIce");
-		AddCard("windturret");
-		AddCard("windturret");
-		AddCard("windturret");
-		AddCard("fireturret");
-		AddCard("fireturret");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
