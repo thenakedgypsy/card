a21952596300caa71296a67851e50de66078a684
Sam <106659952+thenakedgypsy@users.noreply.github.com>
2026-08-19 17:45:17 +0100

    feat: add button to show discard

--- scripts/CardPileDisplayButotn.cs
+++ scripts/CardPileDisplayButotn.cs
@@ -4,20 +4,25 @@ using System.Collections.Generic;
 
 public partial class CardPileDisplayButotn : Button
 {
+	[Export]
+	public string PileName = "Deck";
+	private Cardpile pile;
+	private CardPileDisplay display;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		display = GetTree().GetFirstNodeInGroup("CardPileDisplay") as CardPileDisplay;
+		pile = GetTree().GetFirstNodeInGroup(PileName) as Cardpile;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Text = $"{PileName}: " + pile.GetCards().Count;
 	}
 
 	public void buttonPressed()
 	{
-		Deck deck = GetTree().GetFirstNodeInGroup("Deck") as Deck;
-		CardPileDisplay display = GetTree().GetFirstNodeInGroup("CardPileDisplay") as CardPileDisplay;
-		display.DisplayCards(deck.GetCards() as List<string>);
+		display.DisplayCards(pile.GetCards() as List<string>);
 	}
 }
